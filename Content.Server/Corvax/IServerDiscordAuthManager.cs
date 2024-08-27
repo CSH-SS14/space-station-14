95460102cd6413bb1c070137c67d12a3cb7ea00b
DobriyKaban <MikySato@yandex.ru>
2024-08-27 22:48:05 +0300

    Update IServerDiscordAuthManager.cs

--- Content.Server/Corvax/IServerDiscordAuthManager.cs
+++ Content.Server/Corvax/IServerDiscordAuthManager.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Content.Corvax.Interfaces.Shared;
 using Content.Server._Jerry;
-using Content.Client._Jerry.DiscordAuth;
 using Robust.Server.Player;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
@@ -11,7 +10,7 @@ namespace Content.Corvax.Interfaces.Server;
 
 public interface IServerDiscordAuthManager : ISharedDiscordAuthManager
 {
-    public event EventHandler<ICommonSession>? PlayerVerified;
-    public Task<DiscordAuthManager.DiscordGenerateLinkResponse> GenerateAuthLink(NetUserId userId, CancellationToken cancel);
-    public Task<bool> IsVerified(NetUserId userId, CancellationToken cancel);
+    //public event EventHandler<ICommonSession>? PlayerVerified;
+    //public Task<DiscordAuthManager.DiscordGenerateLinkResponse> GenerateAuthLink(NetUserId userId, CancellationToken cancel);
+    //public Task<bool> IsVerified(NetUserId userId, CancellationToken cancel);
 }
